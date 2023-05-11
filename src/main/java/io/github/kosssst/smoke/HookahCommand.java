ea3cf90d51617697d0d21b36b5c83f5e3f7345d2
Kostia <47629852+kosssst@users.noreply.github.com>
2023-05-11 23:53:13 +0300

    Initial commit

---
+++ src/main/java/io/github/kosssst/smoke/HookahCommand.java
@@ -0,0 +1,2 @@
+package io.github.kosssst.smoke;public class HookahCommand {
+}
---
+++ src/main/java/io/github/kosssst/smoke/SmokePlugin.java
@@ -0,0 +1,17 @@
+package io.github.kosssst.smoke;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmokePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
